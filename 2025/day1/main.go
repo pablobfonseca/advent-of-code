1419a36bd53be8fbeb401ad2f48334d20ddf9ddc
Pablo Fonseca <pablofonseca777@gmail.com>
2025-12-02 20:29:48 +0000

    2025: add first day - part 1

---
+++ 2025/day1/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	StartPosition = 50
+	DialSize      = 100
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatalf("Failed to read input file: %v", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	dialPosition := StartPosition
+	zeroCount := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		direction, distance, err := parseDirection(line)
+		if err != nil {
+			log.Fatalf("Failed to parse input: %v", err)
+		}
+
+		dialPosition = rotateDial(dialPosition, distance, direction)
+
+		if dialPosition == 0 {
+			zeroCount += 1
+		}
+	}
+
+	fmt.Printf("Zero Count: %d\n", zeroCount)
+}
+
+func rotateDial(position, distance int, direction string) int {
+	switch direction {
+	case "L":
+		position -= distance
+	case "R":
+		position += distance
+	}
+
+	return ((position % DialSize) + DialSize) % DialSize
+
+}
+
+func parseDirection(line string) (direction string, value int, err error) {
+	direction = strings.ToUpper(string(line[0]))
+	valString := line[1:]
+
+	value, err = strconv.Atoi(valString)
+	if err != nil {
+		return "", 0, err
+	}
+
+	return
+}
